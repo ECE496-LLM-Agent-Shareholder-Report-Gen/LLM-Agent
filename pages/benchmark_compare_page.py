0acb1ce15d7fad6744653a86b8705da716d92770
Sebastian Czyrny <sebastian.czyrny@mail.utoronto.ca>
2024-03-31 15:21:22 -0400

    moved navbar to own file

--- pages/benchmark_compare_page.py
+++ pages/benchmark_compare_page.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from GUI.benchmark_compare_renderer import BenchmarkCompareRenderer
-from GUI.shared import load_global_singleton, navbar
+from GUI.shared import load_global_singleton
+from GUI.navbar import navbar
 
 if not "global_singleton" in st.session_state:
     global_singleton = load_global_singleton()
